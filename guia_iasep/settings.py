53e72c1e4de78905f3da09ebef18ad6dcbef2598
João Marcelo Freitas de Almeida <jmfda00@gmail.com>
2020-04-25 14:45:57 -0300

    fix typo

--- guia_iasep/settings.py
+++ guia_iasep/settings.py
@@ -125,5 +125,5 @@ STATIC_ROOT = (
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-db_from_env = dj_database_url(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
