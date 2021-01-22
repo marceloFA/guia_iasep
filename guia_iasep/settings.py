a74454d2755ce0132017707cee84c6ecd3667f2e
Marcelo Freitas <jmfda00@gmail.com>
2021-01-22 20:21:26 -0300

    Configura Let's Encrypt

--- guia_iasep/settings.py
+++ guia_iasep/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'guia',
+    'letsencrypt',
 ]
 
 MIDDLEWARE = [
--- guia_iasep/urls.py
+++ guia_iasep/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('guia.urls', namespace='guia')),
+    url(r'^\.well-known/', include('letsencrypt.urls'))
 
 ]
