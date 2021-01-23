1816a550af28c7a0c48f606f648ba2a5168b3223
Marcelo Freitas <jmfda00@gmail.com>
2021-01-22 21:11:03 -0300

    Corrige rotas

--- guia_iasep/urls.py
+++ guia_iasep/urls.py
@@ -2,9 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import url
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('guia.urls', namespace='guia')),
-    url(r'^\.well-known/', include('letsencrypt.urls'))
+from guia import views as guia_views
 
+urlpatterns = [
+    # administrative routes 
+    path('.admin', admin.site.urls),
+    path(r'^\.well-known/', include('letsencrypt.urls')),
+    
+    # medical guide routes
+    path('', guia_views.homepage, name="homepage"),
+    path('<slug:city_slug>', guia_views.city_detail, name="city_detail"),
+    path('<slug:city_slug>/<slug:service_slug>',guia_views.places_listing, name='places_listing'),
 ]
+
